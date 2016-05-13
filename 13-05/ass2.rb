1133237069c5115c736759b9e81aa1d3f25081e4
Krishna <krishna.sahoo@andolasoft.com>
2016-05-13 21:50:57 +0000

    evenodd

--- 13-05/ass2.rb
+++ 13-05/ass2.rb
@@ -2,17 +2,19 @@ begin
 	puts "enter two num: "
 	a= gets
 	b= gets
-	unless a.to_i && b.to_i
+	if a.to_i.to_s != a && b.to_i.to_s != b
 		raise 'An error is occured'
-	else
-		for i in a..b
-			puts "the numbers are: #{i}"
-		end
 	end
-	rescue
-		puts " please enter only number"
-		retry
-	end
+	a=a.to_i
+	b=b.to_i
+	for i in a..b
+		puts "the numbers are: #{i}"
+	end	
+rescue Exception =>e 
+	e.message
+	puts " please enter only number"
+	#retry
+end
 puts ".....Thank you....."
 	
 	
---
+++ 13-05/ass3.rb
@@ -0,0 +1,7 @@
+puts "enter a number: "
+x= gets.to_i
+	if x%2==0
+		puts "number is even"
+	else
+		puts "number is odd"
+	end
